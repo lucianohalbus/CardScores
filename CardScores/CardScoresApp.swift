2e6d6039686d0a4abb60eb0f369a2b850bb50e5b
Luciano <lpuzer@yahoo.com.br>
2024-03-14 11:22:11 -0300

    Initial Commit

---
+++ CardScores/CardScoresApp.swift
@@ -0,0 +1,12 @@
+//Created by Halbus Development
+
+import SwiftUI
+
+@main
+struct CardScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
