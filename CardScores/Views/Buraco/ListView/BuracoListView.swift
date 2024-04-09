f0a8f9d8ef4c1a74d2592c99575c5d8a6cd9e105
Luciano <lpuzer@yahoo.com.br>
2024-04-09 16:03:13 -0300

    Adding plus button to tabview

--- CardScores/Views/Buraco/ListView/BuracoListView.swift
+++ CardScores/Views/Buraco/ListView/BuracoListView.swift
@@ -4,62 +4,52 @@ import SwiftUI
 import FirebaseAuth
 
 struct BuracoListView: View {
-    @State private var isPresented: Bool = false
+    @Binding var addNewMatchIsPresented: Bool
     @EnvironmentObject var buracoListVM: BuracoListViewModel
     @StateObject private var loginVM = LoginViewModel()
     
     var body: some View {
         NavigationStack {
-            VStack {
-                List {
-                    ForEach(buracoListVM.matchesVM) { match in
-                        BuracoCardView(buracoVM: match)
-                            .padding(.bottom, 10)
-                    }
-                    .onDelete(perform: { idxSet in
-                        idxSet.forEach { idx in
-                            let match = buracoListVM.matchesVM[idx]
-                            buracoListVM.delete(matchFB: match)
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(buracoListVM.matchesVM) { match in
+                            BuracoCardView(buracoVM: match)
+                                .padding(.bottom, 10)
                         }
-                    })
-                    .listRowInsets(EdgeInsets.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-                    .listRowBackground(Color.clear)
-                }
-                .scrollContentBackground(.hidden)
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button {
-                            isPresented.toggle()
-                        } label: {
-                            Image(systemName: "plus.circle")
-                                .bold()
-                        }
-                        .buttonStyle(.borderless)
-                        .padding(.trailing, 20)
-                        .tint(Color.cardColor)
-                        .fullScreenCover(isPresented: $isPresented, content: {
-                            AddNewBuracoMatchView()
-                                .interactiveDismissDisabled()
-                                .onDisappear(perform: {
-                                    buracoListVM.getMatches()
-                                })
+                        .onDelete(perform: { idxSet in
+                            idxSet.forEach { idx in
+                                let match = buracoListVM.matchesVM[idx]
+                                buracoListVM.delete(matchFB: match)
+                            }
                         })
+                        .listRowInsets(EdgeInsets.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        .listRowBackground(Color.clear)
                     }
+                    .scrollContentBackground(.hidden)
+                    .fullScreenCover(isPresented: $addNewMatchIsPresented, content: {
+                        AddNewBuracoMatchView()
+                            .interactiveDismissDisabled()
+                            .onDisappear(perform: {
+                                buracoListVM.getMatches()
+                            })
+                    })
+
                 }
-            }
-            .navigationDestination(for: BuracoFBViewModel.self) { item in
-                BuracoMatchView(matchFB: item)
-                    .navigationTitle("")
-            }
-            .listStyle(.insetGrouped)
-            .onAppear {
-                buracoListVM.getMatches()
-            }
-            .onChange(of: loginVM.userAuthenticated) { newValue in
-                if newValue {
+                .navigationDestination(for: BuracoFBViewModel.self) { item in
+                    BuracoMatchView(matchFB: item)
+                }
+                .listStyle(.insetGrouped)
+                .onAppear {
                     buracoListVM.getMatches()
                 }
+                .onChange(of: loginVM.userAuthenticated) { newValue in
+                    if newValue {
+                        buracoListVM.getMatches()
+                    }
+                }
             }
+            .background(Color.cardColor)
         }
     }
 }
--- CardScores/Views/TabView/TabbarView.swift
+++ CardScores/Views/TabView/TabbarView.swift
@@ -5,26 +5,51 @@ import SwiftUI
 struct TabbarView: View {
     
     @Binding var showLoginView: Bool
+    @State var addNewMatchIsPresented: Bool = false
     
     var body: some View {
-        TabView {
-            NavigationStack {
-              BuracoListView()
-            }
-            .tabItem {
-                Image(systemName: "lanyardcard.fill")
-                Text("Buraco")
-            }
-            
-            NavigationStack {
-                ProfileView(showLoginView: $showLoginView)
-            }
-            .tabItem {
-                Image(systemName: "person")
-                Text("Profile")
+        GeometryReader { proxy in
+            ZStack {
+                TabView {
+                    NavigationStack {
+                        BuracoListView(addNewMatchIsPresented: $addNewMatchIsPresented)
+                    }
+                    .tabItem {
+                        Image(systemName: "lanyardcard.fill")
+                        Text("Buraco")
+                    }
+                    
+                    NavigationStack {
+                        ProfileView(showLoginView: $showLoginView)
+                    }
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("Profile")
+                    }
+                }
+                .tint(Color.cardColor)
+                
+                Button (action: {
+                    self.addNewMatchIsPresented = true
+                }) {
+                    ZStack{
+                        
+                        Circle()
+                            .stroke(Color.cardColor, lineWidth: 3)
+                            .frame(width: 55, height: 55)
+                        
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: 100)
+                            .tint(Color.cardColor)
+                    }
+                }
+                .offset(y: proxy.size.height / 2.25)
             }
         }
-        .tint(Color.cardColor)
     }
 }
 
